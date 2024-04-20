f1b6c3f715763edb7457e732b1fc83f46e3e7f05
Marx Alonso <marxchip99@gmail.com>
2024-04-20 17:18:14 -0500

    Correcion de codigo y ordenamiento de carpetas en la carpeta tarea

--- Tareas/Tarea1/Ejercicio1.cpp
+++ Tareas/Tarea1/Ejercicio1.cpp
@@ -2,7 +2,6 @@
 
 int main() {
     float n1, n2, n3;
-
     // Solicitar al usuario que ingrese tres números
     printf("Ingresa el primer número: ");
     scanf("%f", &n1);
@@ -12,20 +11,20 @@ int main() {
     scanf("%f", &n3);
 
     // Ordenar los números de mayor a menor
-    if (num1 >= num2 && num1 >= num3) {
-        if (num2 >= num3) {
+    if (n1 >= n2 && n1 >= n3) {
+        if (n2 >= n3) {
             printf("Los números ordenados de mayor a menor: %.2f, %.2f, %.2f\n", n1, n2, n3);
         } else {
             printf("Los números ordenados de mayor a menor: %.2f, %.2f, %.2f\n", n1, n3, n2);
         }
-    } else if (num2 >= num1 && num2 >= num3) {
-        if (num1 >= num3) {
+    } else if (n2 >= n1 && n2 >= n3) {
+        if (n1 >= n3) {
             printf("Los números ordenados de mayor a menor: %.2f, %.2f, %.2f\n", n2, n1, n3);
         } else {
             printf("Los números ordenados de mayor a menor: %.2f, %.2f, %.2f\n", n2, n3, n1);
         }
     } else {
-        if (num1 >= num2) {
+        if (n1 >= n2) {
             printf("Los números ordenados de mayor a menor: %.2f, %.2f, %.2f\n", n3, n1, n2);
         } else {
             printf("Los números ordenados de mayor a menor: %.2f, %.2f, %.2f\n", n3, n2, n1);
